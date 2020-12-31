3930344a20ac75a9afc7edf96c218fb51a6c3f6f
Tane Piper <tane-piper@idexx.com>
2020-12-31 12:31:09 +0100

    Changelog versioning for libs with files found

--- index.js
+++ index.js
@@ -1,33 +1,84 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const glob = require('@actions/glob');
 
+async function getAffectedLibs(rootDir, baseBranch) {
+    let output = '';
+    const options = {
+        listeners: {
+            stdout: (data) => {
+                output += data.toString();
+            }
+        },
+        cwd: rootDir
+    }
 
-const BASE = 'origin/main~1';
+    await exec.exec(`node_modules/.bin/nx`, ['affected:libs', '--plain', `--base=${baseBranch}`], options);
+    return output.trim().split(' ');
+}
 
-// most @actions toolkit packages have async methods
-async function run() {
-    try {
+function getUpdatedLibs(foundChangelogs) {
+    return foundChangelogs.map(changelog => {
+        const parts = changelog.split('/');
+        return parts[parts.length - 2];
+    })
+}
 
+async function getLibsVersions(rootDir, libFolder, libsWithChangelogs) {
+    const versions = [];
+    const packageFiles = libsWithChangelogs.map(lib => `${rootDir}/${libFolder}/${lib}/package.json`);
+    const packages = await glob.create(packageFiles.join('\n'))
+    const foundPackages = await packages.glob();
+
+    for (const pack of foundPackages) {
         let output = '';
         const options = {
             listeners: {
                 stdout: (data) => {
                     output += data.toString();
                 }
-            }
+            },
         }
+        await exec.exec(`jq`, ['-r', '.version', pack], options);
+        versions.push(output.toString().trim())
+    }
+    return versions;
+}
 
-        await exec.exec('npm', ['run', 'nx', '--', 'affected:libs', '--plain', `--base=${BASE}`], options);
-        core.setOutput('updated', output);
-        console.log(output);
+async function updateChangelog(foundChangelogs, versions) {
+    let count = 0;
+    for (const changelog of foundChangelogs) {
+        const version = versions[count];
+        await exec.exec('sed', ['-i', `s/\\[Unreleased\\]/${version}/`, changelog]);
+    }
+}
+
+
+// most @actions toolkit packages have async methods
+async function run() {
+    try {
+        const rootDir = core.getInput('rootdir', {required: true});
+        const baseBranch = core.getInput('basebranch');
+        const libFolder = core.getInput('libfolder');
+        const fileName = core.getInput('filename');
+        const replaceText = core.getInput('replacement');
+
+        const affected = await getAffectedLibs(rootDir, baseBranch)
+        const changelogFiles = affected.map(lib => `${rootDir}/${libFolder}/${lib}/${fileName}`);
+        const checkExistingChangelogs = await glob.create(changelogFiles.join('\n'))
+        const foundChangelogs = await checkExistingChangelogs.glob();
+
+        const libsWithChangelogs = getUpdatedLibs(foundChangelogs);
+        const versions = await getLibsVersions(rootDir, libFolder, libsWithChangelogs);
+        await updateChangelog(foundChangelogs, versions);
+
+        core.setOutput('updated', libsWithChangelogs);
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
 run();
-
-
 
 
 // const core = require('@actions/core');
--- index.test.js
+++ index.test.js
@@ -1,19 +1,19 @@
-const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
-test('returns a list of items to change', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+    process.env['INPUT_ROOTDIR'] = path.join(__dirname, 'test-workspace');
+    process.env['INPUT_BASEBRANCH'] = 'origin/main';
+    const ip = path.join(__dirname, 'index.js');
+    try {
+        const result = cp.execSync(`node ${ip}`, {env: process.env});
+        const output = result.toString()
+        expect(output).toContain('test-lib-1 test-lib-2')
+    } catch (e) {
+        console.log(e);
+    }
+
+
 })
