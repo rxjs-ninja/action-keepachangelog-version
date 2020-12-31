01d5cadd76b2cc7d5404c8009f9156ba82f53c45
Tane Piper <tane-piper@idexx.com>
2020-12-31 16:48:22 +0100

    Fix for testing

--- index.test.js
+++ index.test.js
@@ -1,23 +1,50 @@
 const process = require('process');
-const cp = require('child_process');
+const {exec, execSync} = require('child_process');
 const path = require('path');
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
+const {promisify} = require('util');
+const {readFile, writeFile} = require('fs');
 
-    process.env['INPUT_ROOTDIR'] = path.join(__dirname, 'test-workspace');
-    process.env['INPUT_BASEBRANCH'] = 'origin/main';
-    process.env['INPUT_LIBFOLDER'] = 'packages';
-    process.env['INPUT_FILENAME'] = 'CHANGELOG.md';
-    process.env['INPUT_REPLACEMENT'] = 'Unreleased';
-    process.env['INPUT_DRYRUN'] = 'true';
-    const ip = path.join(__dirname, 'index.js');
+const readF = promisify(readFile);
+const writeF = promisify(writeFile);
+const execAsync = promisify(exec);
 
 
-    const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-    const lines = result.toString().split('\n');
+describe('NX Keep-A-Changelog Action', () => {
 
-    const parts = lines.find(line => line.includes('name=updated')).split('::');
-    const output = parts[parts.length - 1].trim();
-    expect(output).toStrictEqual('test-lib-1');
+    beforeAll(async () => {
+        const cl = path.join(__dirname, 'test-workspace/packages/test-lib-1/src', 'index.ts');
+        let content = await readF(cl);
+        content = `// New Comment\n` + cl;
+        await writeF(cl, content)
+        await execAsync(`git add . && git commit -m "Add test lib"`)
+    })
+
+
+    beforeEach(() => {
+        process.env['INPUT_ROOTDIR'] = path.join(__dirname, 'test-workspace');
+        process.env['INPUT_BASEBRANCH'] = 'origin/main';
+        process.env['INPUT_LIBFOLDER'] = 'packages';
+        process.env['INPUT_FILENAME'] = 'CHANGELOG.md';
+        process.env['INPUT_REPLACEMENT'] = 'Unreleased';
+        process.env['INPUT_DRYRUN'] = 'true';
+    });
+
+    afterAll(async () => {
+        const cl = path.join(__dirname, 'test-workspace/packages/test-lib-1/src', 'index.ts');
+        await execAsync(`git reset HEAD~1 --soft && git restore ${cl}`);
+    })
+
+
+    // shows how the runner will run a javascript action with env / stdout protocol
+    it('should update a changelog based on changes', () => {
+        const ip = path.join(__dirname, 'index.js');
+        const result = execSync(`node ${ip}`, {env: process.env});
+        const lines = result.toString().split('\n');
+        const parts = lines.find(line => line.includes('name=updated')).split('::');
+        const output = parts[parts.length - 1].trim();
+        expect(output).toStrictEqual('test-lib-1');
+    });
 })
+
+
